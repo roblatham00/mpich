5809272abcc6dbf2ac0bf22eb4ab79f72fce74d9
Adam Moody <moody20@llnl.gov>
2022-02-15 13:05:05 -0800

    test: add fence to perf test

--- src/mpi/romio/test/perf.c
+++ src/mpi/romio/test/perf.c
@@ -158,6 +158,55 @@ int main(int argc, char **argv)
         }
     }
 
+    flag = 0;
+    for (j = 0; j < ntimes; j++) {
+        MPI_CHECK(MPI_File_open(MPI_COMM_WORLD, filename, MPI_MODE_CREATE |
+                                MPI_MODE_RDWR, MPI_INFO_NULL, &fh));
+        MPI_CHECK(MPI_File_seek(fh, mynod * SIZE, MPI_SEEK_SET));
+
+        MPI_Barrier(MPI_COMM_WORLD);
+        stim = MPI_Wtime();
+        MPI_CHECK(MPI_File_write(fh, buf, SIZE, MPI_BYTE, &status));
+        err = MPI_File_fence(fh);
+        write_tim = MPI_Wtime() - stim;
+        if (err == MPI_ERR_UNKNOWN) {
+            flag = 1;
+            break;
+        }
+
+        MPI_CHECK(MPI_File_close(&fh));
+
+        MPI_Barrier(MPI_COMM_WORLD);
+
+        MPI_CHECK(MPI_File_open(MPI_COMM_WORLD, filename, MPI_MODE_CREATE |
+                                MPI_MODE_RDWR, MPI_INFO_NULL, &fh));
+        MPI_CHECK(MPI_File_seek(fh, mynod * SIZE, MPI_SEEK_SET));
+
+        MPI_Barrier(MPI_COMM_WORLD);
+        stim = MPI_Wtime();
+        MPI_CHECK(MPI_File_read(fh, buf, SIZE, MPI_BYTE, &status));
+        read_tim = MPI_Wtime() - stim;
+
+        MPI_CHECK(MPI_File_close(&fh));
+
+        MPI_Allreduce(&write_tim, &new_write_tim, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+        MPI_Allreduce(&read_tim, &new_read_tim, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+
+        min_read_tim = (new_read_tim < min_read_tim) ? new_read_tim : min_read_tim;
+        min_write_tim = (new_write_tim < min_write_tim) ? new_write_tim : min_write_tim;
+    }
+
+    if (mynod == 0) {
+        if (flag)
+            fprintf(stderr, "MPI_File_sync returns error.\n");
+        else {
+            read_bw = (SIZE * nprocs) / (min_read_tim * 1024.0 * 1024.0);
+            write_bw = (SIZE * nprocs) / (min_write_tim * 1024.0 * 1024.0);
+            fprintf(stderr, "Write bandwidth including file fence = %f Mbytes/sec\n", write_bw);
+            fprintf(stderr, "Read bandwidth after file fence = %f Mbytes/sec\n", read_bw);
+        }
+    }
+
     free(buf);
     free(filename);
     MPI_Finalize();
